9d7ceae2ff6829e4cc579e295985c18d61605352
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2021-05-05 00:46:39 +0545

    Popup Menu

--- app/src/main/java/com/example/globalitsupport/activity/HomeActivity.java
+++ app/src/main/java/com/example/globalitsupport/activity/HomeActivity.java
@@ -2,13 +2,20 @@ package com.example.globalitsupport.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import com.example.globalitsupport.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageView gitlogo, popupmenu;
 
     SharedPreferences sharedPreferences;
 
@@ -19,6 +26,33 @@ public class HomeActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.home_layout);
-        
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Userinfo", 0);
+
+        popupmenu = findViewById(R.id.popupmenu);
+        popupmenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopUpMenu(v);
+            }
+        });
+    }
+
+    public void showPopUpMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        getMenuInflater().inflate(R.menu.popupmenu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.logout) {
+                    sharedPreferences.edit().putBoolean("rememnerme", false).apply();
+                    startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                    finish();
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 }
